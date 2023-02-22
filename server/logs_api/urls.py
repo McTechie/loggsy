c4305d6ab0fd987c86446ff7277b544ac932083d
McTechie <mc.soans02@gmail.com>
2023-02-22 18:07:48 +0530

    feat: implement filter logs backend api

--- server/logs_api/urls.py
+++ server/logs_api/urls.py
@@ -3,10 +3,12 @@ from .views import (
     LogListing,
     CreateLog,
     LogDetail,
+    SearchLogs,
 )
 
 urlpatterns = [
     path('logs/', LogListing.as_view()),
     path('log/', CreateLog.as_view()),
     path('log/<int:id>', LogDetail.as_view()),
+    path('logs/search', SearchLogs.as_view()),
 ]
--- server/logs_api/views.py
+++ server/logs_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import Log
 from .serializer import LogSerializer
 
+
 class LogListing(APIView):
     def get(self, request):
         try:
@@ -20,6 +21,7 @@ class LogListing(APIView):
         except Exception as e:
             return Response({ 'message': str(e) })
 
+
 class CreateLog(APIView):
     def post(self, request):
         try:
@@ -30,7 +32,8 @@ class CreateLog(APIView):
             return Response(serializer.errors)
         except Exception as e:
             return Response({ 'message': str(e) })
-        
+
+
 class LogDetail(APIView):
     def get(self, request, id):
         try:
@@ -56,5 +59,31 @@ class LogDetail(APIView):
             log = Log.objects.get(pk=id)
             log.delete()
             return Response({ 'message': 'Log deleted' })
+        except Exception as e:
+            return Response({ 'message': str(e) })
+
+
+class SearchLogs(APIView):
+    def get(self, request):
+        try:
+            filters = {}
+
+            # Filters which can be applied:
+            # If the request data contains 'date_from', then filter the logs by timestamp greater than or equal to it
+            # If the request data contains 'date_to', then filter the logs by timestamp less than or equal to it
+            # If the request data contains any other key ('severity', 'source', etc.), then filter the logs by that key and its value
+
+            for search_param, search_value in request.data.items():
+                if search_param == 'date_from':
+                    filters['timestamp__gte'] = search_value
+                elif search_param == 'date_to':
+                    filters['timestamp__lte'] = search_value
+                else:
+                    filters[search_param] = search_value
+
+            logs = Log.objects.filter(**filters)
+
+            serializer = LogSerializer(logs, many=True)
+            return Response(serializer.data)
         except Exception as e:
             return Response({ 'message': str(e) })
