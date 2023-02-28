c4b7d37628726adc52509642027b03c934fc2318
McTechie <mc.soans02@gmail.com>
2023-02-28 13:01:51 +0530

    fix: minor ui change

--- client/src/components/dashboard/AnnualChart.tsx
+++ client/src/components/dashboard/AnnualChart.tsx
@@ -40,7 +40,7 @@ const AnnualChart: FC<AnnualChartProps> = ({ data }) => {
   }), [labels, values])
 
   return (
-    <div className='shadow-lg rounded-2xl p-5 mx-5 border-4 border-emerald-400 dark:border-indigo-400 bg-white dark:bg-slate-700 flex flex-col items-center justify-center'>
+    <div className='shadow-lg rounded-2xl p-5 border-4 border-emerald-400 dark:border-indigo-400 bg-white dark:bg-slate-700 flex flex-col items-center justify-center'>
       <h3 className='text-lg mb-4 text-center text-gray-500 dark:text-gray-200'>
         Logs created over time
       </h3>
